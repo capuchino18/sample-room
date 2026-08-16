5d59ca8ccf74f90d0d605b4ba12d84dac481b90e
capuchino18 <harindawibi185@gmail.com>
2026-08-17 02:09:10 +0700

    Add global dirname fallback for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+// Solusi darurat agar Vercel mengenali __dirname
+if (typeof globalThis.__dirname === 'undefined') {
+  globalThis.__dirname = process.cwd();
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 };
 
 export default nextConfig;
