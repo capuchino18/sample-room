fa2d39c8607d61adf6dd3fe282d534bafcfa52a2
capuchino18 <harindawibi185@gmail.com>
2026-08-17 11:20:17 +0700

    Remove standalone output to fix Vercel platform 404

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
-if (typeof (global as any).__dirname === 'undefined') {
-  (global as any).__dirname = process.cwd();
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Tambahkan baris ini untuk mengubah mode output
-  output: 'standalone',
 };
 
 export default nextConfig;
